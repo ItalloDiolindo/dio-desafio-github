19c964edbd23ab64420446cd34758b20b4d61ef7
ItalloDiolindo <itallopo@hotmail.com>
2022-04-11 23:25:51 -0300

    Inclusão das anotações de Github e HTML

---
+++ HTML_CSS3/index.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+			<title> MEU PRIMEIRO SITE EM PHP! MASSA DEMAIS!!!</title>
+	</head>
+	
+	<body>
+	
+		<?php
+			for($i = 0 ; $i < 10 ; $i++)
+			{
+				print("Linha número " .$i. "<br />");
+			}
+		?>
+		
+	</body>
+</html>
+			
